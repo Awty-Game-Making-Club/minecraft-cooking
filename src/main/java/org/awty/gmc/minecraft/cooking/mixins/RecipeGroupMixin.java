c3745310a8d405bb91a69f625c1c1172e7ad56c8
matt-mekha <mtmekha8@gmail.com>
2020-10-06 14:15:03 -0500

    logger

--- src/main/java/org/awty/gmc/minecraft/cooking/mixins/RecipeGroupMixin.java
+++ src/main/java/org/awty/gmc/minecraft/cooking/mixins/RecipeGroupMixin.java
@@ -3,8 +3,8 @@ package org.awty.gmc.minecraft.cooking.mixins;
 import net.minecraft.client.recipebook.ClientRecipeBook;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 import net.minecraft.recipe.Recipe;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientRecipeBook.class)
 public class RecipeGroupMixin {
+    private static final Logger logger = LogManager.getLogger("RecipeGroupMixin");
+
     @Inject(method = "getGroupForRecipe", at = @At("RETURN"), cancellable = true)
     private static void customRecipeGroupFixer(Recipe<?> recipe, CallbackInfoReturnable<RecipeBookGroup> callbackInfo) {
         if(callbackInfo.getReturnValue() == RecipeBookGroup.UNKNOWN) {
-            LogManager.getLogger().log(Level.INFO, "Unknown recipe category Mixin fix activated!");
+            logger.info("Unknown recipe category Mixin fix activated!");
             callbackInfo.setReturnValue(RecipeBookGroup.CRAFTING_MISC);
         }
     }
